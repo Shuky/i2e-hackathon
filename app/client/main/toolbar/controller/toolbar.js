73a05619519d5526d4abb8c3c59d259e9dd576bd
Shuky Kappon <shuky.kappon@gmail.com>
2015-11-26 18:34:20 +0200

    Added Image to bar

--- app/client/main/toolbar/controller/toolbar.js
+++ app/client/main/toolbar/controller/toolbar.js
@@ -1,4 +1,8 @@
 angular.module("scpc").controller("ToolbarCtrl", ['$scope', '$meteor',
   function($scope, $meteor){
+	$scope.toggleMenu = false;
+	$scope.onClick = function(){
+		$scope.toggleMenu = !$scope.toggleMenu;
+	}
 
   }]);
